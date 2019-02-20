f244a925662678d95ea05c759c02984b9900bb5a
Vygintas Ranonis <vygintas.ranonis@gmail.com>
2019-02-20 10:57:11 +0000

    improved styling of answer result component

--- src/components/answerResult.js
+++ src/components/answerResult.js
@@ -2,18 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const AnswerResult = ({ isAnswerCorrect, isLastQuestion, onClickFn }) => (
-  <table className="card-overlay">
-    <tbody>
-      <tr>
-        <td className="align-middle">
-          <div className="display-4">{isAnswerCorrect ? 'CORRECT!' : 'WRONG!'}</div>
-          <button type="button" className="btn btn-outline-light rounded-pill" onClick={onClickFn}>
-          {isLastQuestion ? 'View results' : 'Next'}
-          </button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div className="card-overlay">
+    <div className="display-4">{isAnswerCorrect ? 'CORRECT!' : 'WRONG!'}</div>
+    <button type="button" className="btn btn-outline-light rounded-pill" onClick={onClickFn}>
+      {isLastQuestion ? 'View results' : 'Next'}
+    </button>
+  </div>
 );
 
 AnswerResult.propTypes = {
