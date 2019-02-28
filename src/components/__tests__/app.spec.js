c380d2ad2152885759b6ddf3c2e2eca409a2e4e4
Vygintas Ranonis <vygintas.ranonis@gmail.com>
2019-02-28 21:25:21 +0000

    added jest config and tests

---
+++ src/components/__tests__/app.spec.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import App from '../app';
+
+describe('app', () => {
+
+  let wrapper;
+
+  beforeEach(done => {
+    fetch.mockResponse(JSON.stringify([
+      {
+        genre: 'rock',
+        albumCoverUrl: 'http://sex-drugs-and-sausage-rolls',
+        genreOptions: [{ value: 'rock', isCorrect: true}, { value: 'disco', isCorrect: false }]
+      },
+      {
+        genre: 'opera',
+        albumCoverUrl: 'http://pavarotti-lives',
+        genreOptions: [{ value: 'opera', isCorrect: true}, { value: 'techno', isCorrect: false }]
+      }
+    ]));
+    wrapper = mount(<App/>);
+    process.nextTick(() => {
+      wrapper.find('button').simulate('click');
+      fetch.mockClear()
+      done();
+    });
+  });
+
+  it('should display the game when starting', () => {
+    expect(wrapper.find('Game')).toMatchSnapshot();
+  });
+
+  it('should indicate when a question is answered correctly', () => {
+    wrapper.find({ answer: { isCorrect: true }}).simulate('click');
+    expect(wrapper.find('AnswerResult')).toMatchSnapshot();
+  });
+
+  it('should indicate when a question is answered incorrectly', () => {
+    wrapper.find({ answer: { isCorrect: false }}).simulate('click');
+    expect(wrapper.find('AnswerResult')).toMatchSnapshot();
+  });
+
+  it('should display the game result when all questions are answered, and restart the game', done => {
+    wrapper.find({ answer: { isCorrect: false }}).simulate('click');
+    wrapper.find('button').simulate('click');
+    wrapper.find({ answer: { isCorrect: true }}).simulate('click');
+    wrapper.find('button').simulate('click');
+    expect(wrapper.find('GameResult')).toMatchSnapshot();
+
+    fetch.mockResponse(JSON.stringify([
+      {
+        genre: 'country',
+        albumCoverUrl: 'http://been-cleaning-the-barn',
+        genreOptions: [{ value: 'country', isCorrect: true}, { value: 'pop', isCorrect: false }]
+      }
+    ]));
+
+    wrapper.find('button').simulate('click');
+    process.nextTick(() => {
+      wrapper.update();
+      expect(wrapper.find('Game')).toMatchSnapshot();
+      done();
+    })
+  });
+});
